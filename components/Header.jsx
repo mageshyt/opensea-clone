bc5eff76fac6f2d35df6033aa72a3ed9d03df51c
magesh <hariarunengg@gmail.com>
2022-02-02 15:30:50 +0530

    header setup finished

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,13 +6,14 @@ import { AiOutlineSearch } from 'react-icons/ai'
 import { CgProfile } from 'react-icons/cg'
 import { MdOutlineAccountBalanceWallet } from 'react-icons/md'
 const style = {
-  wrapper: `bg-[#04111d] w-screen px-[1.2rem] py-[0.8rem] flex `,
+  wrapper: `bg-[#04111d] w-screen px-[1.2rem] flex   justify-between  py-[0.8rem]  `,
   logoContainer: `flex items-center cursor-pointer`,
   logoText: ` ml-[0.8rem] text-white font-semibold text-2xl`,
-  searchBar: `flex flex-1 mx-[0.8rem] w-max-[520px] items-center bg-[#363840] rounded-[0.8rem] hover:bg-[#4c505c]`,
+  searchBar: `flex flex-1 hidden sm:flex mx-[0.8rem] w-max-[520px] items-center bg-[#363840] rounded-[0.8rem] hover:bg-[#4c505c]`,
   searchIcon: `text-[#8a939b] mx-3 font-bold text-lg`,
   searchInput: `h-[2.6rem] w-full border-0 bg-transparent outline-0 ring-0 px-2 pl-0 text-[#e6e8eb] placeholder:text-[#8a939b]`,
-  headerItems: ` flex items-center justify-end`,
+  headerItems: ` flex items-center justify-end hidden lg:flex`,
+  headerItemsIcons: ` flex items-center justify-evenly   `,
   headerItem: `text-white px-4 font-bold text-[#c8cacd] hover:text-white cursor-pointer`,
   headerIcon: `text-[#8a939b] text-3xl font-black px-4 hover:text-white cursor-pointer`,
 }
@@ -36,6 +37,25 @@ const Header = () => {
           className={style.searchInput}
           placeholder="Search items, collections"
         />
+      </div>
+      {/* Header items */}
+      <div className={style.headerItems}>
+        {/* Items */}
+
+        <div className={style.headerItem}> Collections </div>
+        <div className={style.headerItem}> Stats </div>
+        <div className={style.headerItem}> Resources </div>
+        <div className={style.headerItem}> Create </div>
+      </div>
+
+      {/* Icons */}
+      <div className={style.headerItemsIcons}>
+        <div className={style.headerIcon}>
+          <CgProfile />
+        </div>
+        <div className={style.headerIcon}>
+          <MdOutlineAccountBalanceWallet />
+        </div>
       </div>
     </div>
   )
