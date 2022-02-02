77de211903e6ac963995a9f95f2c2631280ce6b7
magesh <hariarunengg@gmail.com>
2022-02-02 15:12:13 +0530

    header setup

---
+++ pages/index.jsx
@@ -0,0 +1,5 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+export default function Home() {
+  return <Header />
+}
--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import Head from 'next/head'
-
-export default function Home() {
-  return (
-    <div className="flex h-screen w-full items-center justify-center bg-black text-5xl text-white ">
-      <h1>Hello</h1>
-    </div>
-  )
-}
